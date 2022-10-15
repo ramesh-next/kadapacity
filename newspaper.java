4942b3af1487dbac400e71d51eee031229eaeaba
arsramesh <arsramesh000@gmail.com>
2022-10-15 15:56:23 +0530

    third commit

--- newspaper.java
+++ newspaper.java
@@ -1,3 +1,4 @@
 newspaper is some peoples are read in daily habbits
 in the news are somany content and information in the daily
-like somany information are choose in the 
+like somany information are choose in the
+in the paricular 

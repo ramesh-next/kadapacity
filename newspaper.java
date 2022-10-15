b1ff8f14355f3b6836bde2b032d5eef7e60771d7
arsramesh <arsramesh000@gmail.com>
2022-10-15 16:09:18 +0530

    commit change

--- newspaper.java
+++ newspaper.java
@@ -2,3 +2,5 @@ newspaper is some peoples are read in daily habbits
 in the news are somany content and information in the daily
 like somany information are choose in the
 in the paricular 
+aws tool are teaching late starting from the class
+ansible are only the 
